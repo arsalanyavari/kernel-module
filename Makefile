7683d0f08bd671b247517eae8417a6132e29502c
arsalanyavari <arya48.yavari79@gmail.com>
2022-01-17 17:46:30 +0330

    project compleated

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 obj-m = iutmodule.o
-.DEFAULT_GOAL := all
-all:
-	@echo "$(tput setaf 0) $(tput setab 2) making install :) $(tput sgr 0)"
+.DEFAULT_GOAL := install
+install:
+	@echo "$(tput setaf 0) $(tput setab 3) Making install :) $(tput sgr 0)"
 	@make -C /lib/modules/$(shell uname -r)/build/ M=$(PWD) modules
 clean:
-	@echo "$(tput setaf 0) $(tput setab 1) making clean :( $(tput sgr 0)"
+	@echo "$(tput setaf 0) $(tput setab 9) Making clean :( $(tput sgr 0)"
 	@make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
--- pytest.py
+++ pytest.py
@@ -3,102 +3,74 @@ import threading as th
 from random import randint
 
 
-
-
-def enteghal_action(from_:str, to_:str, amount:str):
+def enteghal_action(from_, to_, amount):
     return f"e,{from_},{to_},{amount}"
 
-def variz_action(to_:str, amount:str):
+
+def variz_action(to_, amount):
     return f"v,-,{to_},{amount}"
 
-def bardasht_action(from_:str, amount:str):
+
+def bardasht_action(from_, amount):
     return f"b,{from_},-,{amount}"
+
 
 def reset_action():
     return "r"
 
-def read_from_file(fd:int):
+
+def read_from_file(fd: int):
     print("READ DATA FROM FILE : \n")
     init_data = os.read(fd, 1024)
     print(init_data.decode())
 
 
-
-def write_to_file(fd:int, cmd:str):
+def write_to_file(fd: int, cmd: str):
     os.write(fd, cmd.encode())
 
 
-
-def gateway_1(fd:int):
-
+def gateway_1(fd: int):
     for i in range(100):
-        print("in 2th thread : " + str(i))
-        dis_acnt = randint(1,99)
+        dis_acnt = randint(1, 99)
         cmd = enteghal_action(str(dis_acnt), '0', 1)
-        print(cmd)
         write_to_file(fd, cmd)
-        print("in 2th thread : " + str(i))
 
 
-def gateway_2(fd:int):
-
+def gateway_2(fd: int):
     for i in range(100):
-        print("in 2th thread : " + str(i))
-        dis_acnt = randint(1,99)
+        dis_acnt = randint(1, 99)
         cmd = enteghal_action(str(dis_acnt), '0', 1)
-        print(cmd)
         write_to_file(fd, cmd)
-        print("in 2th thread : " + str(i))
 
 
-
-
-
-if __name__ == "__main__":
+def main():
     print(">>>>>>>>>>>>start<<<<<<<<<<<\n")
     path = "/dev/arnanode"
     print("OPEN FILE\n")
 
-    #open file
+    # open file
     fd = os.open(path, os.O_RDWR)
 
-    #read file
+    # read file
     read_from_file(fd)
 
     print("log1")
-    t1 = th.Thread(target= gateway_1, args=(fd,))
-    t2 = th.Thread(target= gateway_2, args=(fd,))
+    t1 = th.Thread(target=gateway_1, args=(fd,))
+    t2 = th.Thread(target=gateway_2, args=(fd,))
     print("log2")
-    #start two threades
+
+    # start two threades
     t1.start()
     t2.start()
     t1.join()
     t2.join()
-    #end two threades
 
+    # end two threades
     read_from_file(fd)
-    
-
     os.close(fd)
-    print("\n\n<<<<<<<<<<<<<<<<THE END>>>>>>>>>>>>>>>>>>>\n\n\n")
+    print("\n\n\t\t\t NAVID & ARSALAN")
+    print("\t\t<<<<<<<<<<<<<THE END>>>>>>>>>>>\n\n\n")
 
 
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
